cd1b108bd28b3a137f36414ee12b4acbd33dd5ed
Vicki Hamm <vicki.cello@gmail.com>
2018-08-18 16:59:56 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -5,6 +5,8 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
       t.string :name
       t.integer :show_id
       t.integer :actor_id
+      t.belongs_to :actor, index: true
+      t.belongs_to :show, index: true
     end
   end
 
